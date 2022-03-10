57701ee736e84954a32cee1f58d653b9559142fa
ZiongaB <91677985+ZiongaB@users.noreply.github.com>
2022-03-10 09:21:14 -0500

    Base Project
    Here's the skeleton

---
+++ VRAssignment2/Assets/Scripts/Original Scripts/Spinny.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinny : MonoBehaviour
+{
+    public Animator Anime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void spin()
+    {
+        Anime.Play("NewSpin");
+    }
+}
